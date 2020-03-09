e1f60fa260b3c8c0569985a0ed09433d093e7135
kkuop <60794860+kkuop@users.noreply.github.com>
2020-03-09 11:21:52 -0500

    completed problem 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -10,6 +10,21 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            //*******************
+            // PROBLEM 1
+            //*******************
+            List<string> words = new List<string>() {"the", "bike", "this", "it", "tenth", "mathematics" };
+            var newList = words.Where(m => m.Contains("th"));
+            foreach (string w in newList)
+            {
+                Console.WriteLine(w);
+            }
+            Console.ReadKey();
+
+            //*******************
+            // PROBLEM 2
+            //*******************
+
         }
     }
 }
