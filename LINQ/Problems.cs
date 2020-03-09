115d6a6906edf9ea4727efa8f1621d30c2446a5e
kkuop <60794860+kkuop@users.noreply.github.com>
2020-03-09 11:47:02 -0500

    created problem class and will test in the program class

---
+++ LINQ/Problems.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Problems
+    {
+        public void PrintNewList(List<string> words)
+        {
+            var newList = words.Where(m => m.Contains("th"));
+            foreach (string w in newList)
+            {
+                Console.WriteLine(w);
+            }
+        }
+    }
+}
